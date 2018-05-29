093f4e1fe0854a4e048755f3b0d8c2eb79da1621
oviedo97fer <37948156+oviedo97fer@users.noreply.github.com>
2018-05-29 00:13:29 -0300

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var container = document.getElementById('container-load');
+    container.classList.add('cerrar');
+    //var load = document.getElementById('load');
+    //load.classList.add('load');
+};
